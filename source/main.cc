cdb1c1b9168c4401b61ffbd0e50be475aed21261
Shayna Kaine <Fedorable@protonmail.ch>
2019-02-16 14:43:23 -0300

    Added main entrypoint

---
+++ source/main.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include <vector>
+#include <functional>
+#include <array>
+#include <map>
+
+#include "headers/MouseLocker.hh"
+#include "headers/InputChecker.hh"
+#include "headers/ProcessChecker.hh"
+#include "headers/ParamParser.hh"
+#include "headers/UserInterface.hh"
+
+int main(int argc, char* argv[]) {
+
+    std::cout << "Press enter to exit." << std::endl;
+    std::cin.get();
+    return 0;
+}
