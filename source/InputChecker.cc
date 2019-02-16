0be32eca2e662e4dfd214a47ffc096ae49027cc1
Shayna Kaine <Fedorable@protonmail.ch>
2019-02-16 14:42:20 -0300

    Added new implementation of IConditonChecker, called InputChecker

---
+++ source/InputChecker.cc
@@ -0,0 +1,39 @@
+#include "headers/InputChecker.hh"
+
+void InputChecker::check_() {
+    while(!this->thread_terminate) {
+        if((this->thread_enabled) && ((GetAsyncKeyState(this->settings.virtual_keycode) & 0x8000) != 0)) {
+            bool state = (this->mouseLocker_.thread_enabled ^= true);
+            if(this->settings.play_sound)  Beep(state ? 900 : 700, 30);
+
+            Sleep(500);
+        }
+        
+        Sleep(60);
+    }
+}
+
+void InputChecker::kill() {
+    this->thread_enabled = false;
+    this->thread_terminate = true;
+
+    this->check_thread_.join();
+}
+
+InputChecker::InputChecker(uint8_t vkid) : IConditionChecker() {
+    this->thread_enabled = false;
+    this->thread_terminate = false;
+
+    this->settings.mlsettings = &(this->mouseLocker_.settings);
+    this->settings.virtual_keycode = vkid;
+    this->settings.polling_rate = 60;
+    this->settings.input_delay = 500;
+    this->settings.play_sound = true;
+
+    this->check_thread_ = std::thread(&InputChecker::check_, this);
+}
+
+InputChecker::~InputChecker() {
+    std::cout << "InputChecker destructor called." << std::endl;
+    kill();
+}
---
+++ source/headers/InputChecker.hh
@@ -0,0 +1,42 @@
+#ifndef INPUTCHECKER
+#define INPUTCHECKER
+
+#include <iostream> // remove this include
+#include <Windows.h>                // Windows API functions.
+#include <thread>                   // Multi-threading support.
+#include <cstdint>                  // Alternative integer definitions.
+
+#include "IConditionChecker.hh"     // IConditionChecker interface / superclass..
+
+/**
+ * This is an implementation of the IConditionChecker interface. This implementation focuses on checking
+ * for user input. The check_ pure virtual method is overriden with a method that asynchronously checks
+ * for a specific key being pressed. This key is specified through the virtual_keycode setting.
+ * This variable should be a VKID (Virtual Key ID). Uppon being pressed, it will toggle the mouseLock_ thread.
+ * List of valid VKIDs: http://cherrytree.at/misc/vk.htm
+ */
+
+class InputChecker : public IConditionChecker {
+protected:
+    void check_() override;         // The overriden virtual method that will contain the condition checking code.
+
+public:
+    bool thread_enabled;            // Bool that when true, enables the functionality of the check_ method. When false, check_ keeps running in a loop, but does nothing.
+    bool thread_terminate;          // Bool that when true, stops the loop in the check_ method, allowing check_ to return.
+
+    struct Settings : ISettings {
+        uint8_t virtual_keycode;    // The virtual keycode of the key that should be checked by the check_ method.
+
+        uint16_t polling_rate;      // How frequently (in milliseconds) the check_ method should be shecking for input.
+        uint16_t input_delay;       // The delay after an input is registered before continuing to check. Helps avoid over-sensitivity.
+
+        bool play_sound;            // Whether or not to play a sound when input is registered.
+    } settings;
+
+    void kill();    // Method that sets thread_enabled to false, and thread_terminate to true, and joins the check_thread_ in the superclass.
+
+    InputChecker(uint8_t);
+    ~InputChecker() override;
+};
+
+#endif
