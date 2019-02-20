c5874f75226138c38b6e895acdaa08b2709b5dcf
Shayna Kaine <Fedorable@protonmail.ch>
2019-02-19 23:59:48 -0300

    Is now a fully functioning class.
    Added new dialogues, methods, and functionality.
    
    Switched to C++ type casting from C casting.

--- source/UiCore.cc
+++ source/UiCore.cc
@@ -1,5 +1,38 @@
 #include "headers/UiCore.hh"
 
+template<typename T>
+bool UiCore::AutoNumericalInput(const std::string& string, T* output) const {
+    uint32_t output_buffer;
+
+    if(string.size() > 2 && (string.at(0) == '0' && string.at(1) == 'x')) {
+        try {
+            std::stringstream conversion_buffer;
+            conversion_buffer << std::hex;
+            conversion_buffer << string;
+            conversion_buffer >> output_buffer;
+        } catch(std::exception& exception) { 
+            std::cout << "Failed to convert hex \"" << string << "\" to an unsigned 32-Bit integer." << std::endl;
+            return false;
+        }
+    } else {
+        try {
+            output_buffer = std::stoi(string);
+        } catch(std::exception& exception) {
+            std::cout << "Failed to convert decimal \"" << string << "\" to an unsigned 32-Bit integer." << std::endl;
+            return false;
+        }
+    }
+
+    if(output_buffer <= pow(256, sizeof(T))) {
+        *output = static_cast<T>(output_buffer);
+    } else {
+        std::cout << "Converted number exceeds maximum type size (" << output_buffer << " > " << pow(256,sizeof(T)) << ")" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
 void UiCore::SetConsoleBufferSize(int16_t cols, int16_t rows) const {
     _COORD new_buffer_size = {cols, rows};
     void* console_buffer_handle = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -23,6 +56,155 @@ void UiCore::LockConsoleSize() const {
     int32_t old_window_style = GetWindowLongPtr(console_window_handle, GWL_STYLE);
     int32_t new_window_style = old_window_style ^ WS_MAXIMIZEBOX ^ WS_SIZEBOX;
     SetWindowLongPtr(console_window_handle, GWL_STYLE, new_window_style);
+}
+
+void UiCore::Dialogue_ProcessChecker(ProcessChecker** pc_target) const {
+    while(true) {
+        system("cls");
+        std::cout << "1.) Image Name: " << (*pc_target)->settings.imageName << std::endl;
+        std::cout << "2.) Polling Rate: " << (*pc_target)->settings.polling_rate << "ms" << std::endl;
+        std::cout << "3.) Locking Rate: " << (*pc_target)->settings.mlsettings->setcursor_interval << "ms" << std::endl;
+        std::cout << "4.) On/Off Sound: " << (((*pc_target)->settings.play_sound) ? "Enabled" : "Disabled") << std::endl;
+
+        std::cout << std::endl << "0.) Return" << std::endl << std::endl;
+
+        char selection = '\0';
+
+        while(!(selection >= '0' && selection <= '4'))
+            selection = _getch();
+
+        switch(selection) {
+            case '0' : {
+                return;
+            }
+
+            case '1' : {
+                std::cout << "New name ~ ";
+                std::string new_value;
+                std::cin >> new_value;
+                
+                realloc((*pc_target)->settings.imageName, strlen(new_value.c_str()));
+                strcpy((*pc_target)->settings.imageName, new_value.c_str());
+                break;
+            }
+
+            case '2' : {
+                std::cout << "New rate(ms) ~ ";
+                std::string string_input;
+                std::cin >> string_input;
+
+                uint16_t new_value;
+
+                if(AutoNumericalInput<uint16_t>(string_input, &new_value)) {
+                    (*pc_target)->settings.polling_rate = new_value;
+                } else {
+                    std::cout << "Press any key to continue." << std::endl;
+                    _getch();
+                }
+                break;
+            }
+
+            case '3' : {
+                std::cout << "New rate(ms) ~ ";
+                std::string string_input;
+                std::cin >> string_input;
+
+                uint16_t new_value;
+
+                if(AutoNumericalInput<uint16_t>(string_input, &new_value)) {
+                    (*pc_target)->settings.mlsettings->setcursor_interval = new_value;
+                } else {
+                    std::cout << "Press any key to continue." << std::endl;
+                    _getch();
+                }
+                break;
+            }
+
+            case '4' : {
+                (*pc_target)->settings.play_sound ^= true;
+                break;
+            }
+        }
+    }
+}
+
+void UiCore::Dialogue_InputChecker(InputChecker** ic_target) const {
+    while(true) {
+        system("cls");
+        std::cout << "1.) Trigger VKID: " << std::uppercase << std::hex << static_cast<uint32_t>((*ic_target)->settings.virtual_keycode) << std::dec << std::endl;
+        std::cout << "2.) Polling Rate: " << (*ic_target)->settings.polling_rate << "ms" << std::endl;
+        std::cout << "3.) Locking Rate: " << (*ic_target)->settings.mlsettings->setcursor_interval << "ms" << std::endl;
+        std::cout << "4.) On/Off Sound: " << (((*ic_target)->settings.play_sound) ? "Enabled" : "Disabled") << std::endl;
+
+        std::cout << std::endl << "0.) Return" << std::endl << std::endl;
+
+        char selection = '\0';
+
+        while(!(selection >= '0' && selection <= '4')) 
+            selection = _getch();
+
+        switch(selection) {
+            case '0' : {
+                return;
+            }
+
+            case '1' : {
+                uint8_t value_buffer;
+
+                std::cout << "New VKID ~ ";
+                std::string string_input;
+                std::cin >> string_input;
+
+                if(AutoNumericalInput<uint8_t>(string_input, &value_buffer)) {
+                    (*ic_target)->settings.virtual_keycode = value_buffer;
+                } else {
+                    std::cout << "Press any key to continue." << std::endl;
+                    _getch();
+                }
+
+                break;
+            }
+
+            case '2' : {
+                uint16_t value_buffer;
+
+                std::cout << "New rate(ms) ~ ";
+                std::string string_input;
+                std::cin >> string_input;
+
+                if(AutoNumericalInput<uint16_t>(string_input, &value_buffer)) {
+                    (*ic_target)->settings.polling_rate = value_buffer;
+                } else {
+                    std::cout << "Press any key to continue." << std::endl;
+                    _getch();
+                }
+
+                break;
+            }
+
+            case '3' : {
+                uint16_t value_buffer;
+
+                std::cout << "New rate(ms) ~ ";
+                std::string string_input;
+                std::cin >> string_input;
+
+                if(AutoNumericalInput<uint16_t>(string_input, &value_buffer)) {
+                    (*ic_target)->settings.mlsettings->setcursor_interval = value_buffer;
+                } else {
+                    std::cout << "Press any key to continue." << std::endl;
+                    _getch();
+                }
+
+                break;
+            }
+
+            case '4' : {
+                (*ic_target)->settings.play_sound ^= true;
+                break;
+            }
+        };
+    }
 }
 
 void UiCore::Dialogue_SelectMode(IConditionChecker** polymorphic_target, bool optional) const {
@@ -63,15 +245,16 @@ void UiCore::Dialogue_SelectMode(IConditionChecker** polymorphic_target, bool op
 }
 
 void UiCore::Dialogue_Main() const {
-    IConditionChecker* icc_polymorphic = new ProcessChecker("tesv.exe");
-    this->Dialogue_SelectMode(&icc_polymorphic, false);
+    IConditionChecker* ic_polymorphic = new ProcessChecker("tesv.exe");
+    
+    this->Dialogue_SelectMode(&ic_polymorphic, false);
     bool exit_loop = false;
 
     while(!exit_loop) {
         system("cls");
         std::cout << "1.) Mode: ";
 
-        switch(icc_polymorphic->ID) {
+        switch(ic_polymorphic->ID) {
             case IConditionChecker::ICCID : {
                 std::cout << "(ERROR) BASE POLYMORPHIC TYPE." << std::endl;
                 break;
@@ -93,7 +276,7 @@ void UiCore::Dialogue_Main() const {
             }
         }
 
-        std::cout << "2.) State: " << (icc_polymorphic->thread_enabled  ? "Enabled" : "Disabled") << std::endl;
+        std::cout << "2.) State: " << (ic_polymorphic->thread_enabled  ? "Enabled" : "Disabled") << std::endl;
         std::cout << "3.) Configure" << std::endl;
         std::cout << std::endl << "0.) Exit" << std::endl;
 
@@ -103,17 +286,32 @@ void UiCore::Dialogue_Main() const {
 
         switch(selection) {
             case '1' : {
-                this->Dialogue_SelectMode(&icc_polymorphic, true);
+                this->Dialogue_SelectMode(&ic_polymorphic, true);
                 break;
             }
 
             case '2' : {
-                icc_polymorphic->thread_enabled ^= true;
+                ic_polymorphic->thread_enabled ^= true;
                 break;
             }
 
             case '3' : {
-                // TODO: Implement configure functions once those dialogues are complete.
+                switch(ic_polymorphic->ID) {
+                    case IConditionChecker::ICCID : {
+                        break;
+                    }
+
+                    case InputChecker::ICID : {
+                        this->Dialogue_InputChecker(reinterpret_cast<InputChecker**>(&ic_polymorphic));
+                        break;
+                    }
+
+                    case ProcessChecker::PCID : {
+                        this->Dialogue_ProcessChecker(reinterpret_cast<ProcessChecker**>(&ic_polymorphic));
+                        break;
+                    }
+                };
+
                 break;
             }
 
@@ -129,9 +327,7 @@ void UiCore::Dialogue_Main() const {
         }
     }
 
-    
-    icc_polymorphic->~IConditionChecker();
-    free(icc_polymorphic);
+    delete ic_polymorphic;
 }
 
 UiCore::UiCore() {
--- source/headers/UiCore.hh
+++ source/headers/UiCore.hh
@@ -2,24 +2,72 @@
 #define UICORE
 
 #include <Windows.h>
+#include <sstream>
+#include <string>
 #include <cstdint>
 #include <conio.h>
+#include <cmath>
 
 #include "IConditionChecker.hh"
 #include "InputChecker.hh"
 #include "ProcessChecker.hh"
 
+/**
+ * This class is responsible for the user interface, and is what the user interacts with when launching the program.
+ * It imeplements the polymorphic IConditionChecker class, and handles the polymorphism. 
+ * 
+ * The interface itself is split among different Dialogue methods, each drawing a different dialogue.
+ * Each dialogue is responsible for drawing, recieving input, and making requested alterations to 
+ * the configuration.
+ * 
+ * Accompanying the dialogue methods, are some helper methods that the dialogues use to shorten code.
+ * e.g. AutoNumericalInput.
+ * 
+ * The only public Dialogue method is Dialogue_Main() which automatically calls the other dialogue methods,
+ * and is virtually an "entrypoint" to the dialogue tree.
+ * 
+ * ! Some methods aren't ever called, this is due to a mid-way change in structure. 
+ * ! These methods are left in, as they're lightweight and may be used in future versions.
+ */
+
 class UiCore {
 protected:
+
+    /**
+     * This converts a string representation of a number, in either decimal or hexadecimal format, into an integer.
+     * It takes the integer type as a template argument, and also takes in a pointer to where the output should be stored.
+     * If a problem is encountered, it will print the problem to the console before returning false. If no errors
+     * are encountered, the method returns true. 
+     * 
+     * Hexadecimal numbers are detected through the 0x prefix.
+     */
+    template<typename T>
+    bool AutoNumericalInput(const std::string&, T*) const;
+    
+    // Sets the size of the console buffer.
     void SetConsoleBufferSize(int16_t, int16_t) const;
+
+    // Sets the size of the console window.
     void SetConsoleWindowSize(int16_t, int16_t) const;
+
+    // Moves the console cursor to a specified row and column.
     void MoveCursor(int16_t, int16_t) const;
+
+    // Disables the ability to resize the console window.
     void LockConsoleSize() const;
 
-public:
-    void Dialogue_ProcessChecker() const;
-    void Dialogue_InputChecker() const;
+    // The dialogue for configuring the ProcessChecker type.
+    void Dialogue_ProcessChecker(ProcessChecker**) const;
+
+    // The dialogue for configuring the InputChecker type.
+    void Dialogue_InputChecker(InputChecker**) const;
+
+    // The dialogue for selecting and assigning a subclass to the polymorphci IConditionChecker type.
     void Dialogue_SelectMode(IConditionChecker**, bool) const;
+
+public:
+
+    // The main dialogue, where the other dialogues. This is the starting point of this class.
     void Dialogue_Main() const;
 
     UiCore();
