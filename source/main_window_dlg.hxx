ab349c2407da72ff23220f169c7bacfb88ae9a77
Shayna Kaine <0x0000@tuta.io>
2019-10-26 21:26:47 -0300

    Programmed Ui.

--- source/main_window_dlg.hxx
+++ source/main_window_dlg.hxx
@@ -1,7 +1,22 @@
 #ifndef MAIN_WINDOW_DLG_HXX
 #define MAIN_WINDOW_DLG_HXX
 
+#undef UNICODE
+
 #include <QMainWindow>
+
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <thread>
+#include <atomic>
+#include <mutex>
+
+#include <algorithm>
 
 namespace Ui {
     class MainWindow;
@@ -12,7 +27,34 @@ private:
     Ui::MainWindow* ui;
     Q_OBJECT
 
+    std::atomic<uint32_t> target_position_x;
+    std::atomic<uint32_t> target_position_y;
+
+    std::atomic<bool> cursor_locker_activated;
+    [[noreturn]] void cursor_locker_worker();
+
+    std::atomic<bool> activation_mode;
+    std::atomic<uint32_t> numerical_vkid;
+    [[noreturn]] void activator_worker();
+
+    std::mutex console_mutex;
+
+    void log_to_console(const QList<QString>&);
+
+private slots:
+    void update_frequency_group_title();
+    void update_resolution_center();
+
+    void invert_lock_state();
+    void set_lock_state(bool);
+
+    void on_btn_edit_activation_clicked();
+    void on_cbx_activation_currentIndexChanged(int);
+
 public:
+    std::thread cursor_locker_thread;
+    std::thread activator_thread;
+
     explicit MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 };
